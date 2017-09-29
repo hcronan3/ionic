bef2f975fd92684561329e10a5d714855a2010d5
hcronan3 <hcronan@stu.jsu.edu>
2017-09-29 11:45:22 -0500

    alert coordinates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -45,7 +45,7 @@ export class HomePage {
         console.log("in then");
  // resp.coords.latitude
  // resp.coords.longitude
-    console.log(resp.coords.latitude);
+    alert(resp.coords.latitude, resp.coords.longitude);
 }).catch((error) => {
   console.log('Error getting location', error);
 });
